2b439357edba4d8eabdd62b5113197d2f9bfd151
77claralunagomez <claragomez968@gmail.com>
2025-04-24 18:55:42 -0300

    agregue mi boton de whatapp al footer

--- app/Views/footer.php
+++ app/Views/footer.php
@@ -1,9 +1,22 @@
 <footer>
-    <p class="text-light">
-        Todos los derechos reservados. Todas las marcas registradas pertenecen a sus respectivos dueños
-        en EE. UU. y otros países.
-        Todos los precios incluyen IVA (donde sea aplicable).
-    </p>
+<div class="boton-flota" style="position: fixed; bottom: 20px; right: 20px; z-index: 1000;">
+        <div class="separator" style="clear: both; text-align: start;">
+            <nav>
+                <ul id="navegacion">
+                    <li class="btn-group dropup"> <!-- Para que el menú se despliegue hacia arriba -->
+                        <button type="button" class="btn dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false" style="border: none;">
+                            <img src="assets/img/Whatsapp.png" alt="WhatsApp" style="width: 40px; height: 40px;">
+                        </button>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="https://wa.me/5493794060957/">Post Venta</a></li>
+                            <li><a class="dropdown-item" href="https://wa.me/5493794060957/">Reparaciones</a></li>
+                            <li><a class="dropdown-item" href="https://wa.me/5493794060957/">Mayorista</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
 </footer>
 
 <script src="assets/js/bootstrap.bundle.min.js"></script>
