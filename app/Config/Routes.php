c347438d5776fae26819f0cd2dd9b018f536bdc1
Mauren <mauren3312@gmail.com>
2025-04-23 22:28:44 -0300

    agregadas rutas de controladores

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('contacto', 'Home::contacto');
+$routes->get('comercializacion', 'Home::comercializacion');
+$routes->get('aboutUs', 'Home::aboutUs');
+$routes->get('terminos', 'Home::terminos');
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,8 +4,43 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index(): string
+    public function index()
     {
-        return view('principal.php');
+        echo view('head');
+        echo view('navbar');
+        echo view('index');
+        echo view('comercializacion');
+        echo view('contacto');
+        echo view('aboutUs');
+        echo view('footer');
+    }
+    public function contacto(){
+        echo view('head');
+        echo view('navbar');
+        echo view('contacto');
+        echo view('footer');
+    }
+    public function comercializacion(){
+        echo view('head');
+        echo view('navbar');
+        echo view('comercializacion');
+        echo view('footer');
+
+    }
+
+    public function aboutUs(){
+        echo view('head');
+        echo view('navbar');
+        echo view('aboutUs');
+        echo view('footer');
+
+    }
+
+    public function terminos(){
+        echo view('head');
+        echo view('navbar');
+        echo view('terminos');
+        echo view('footer');
+
     }
 }
