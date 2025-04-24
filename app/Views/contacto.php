3146c847f6c10db68b8533bd9894c56adb796a62
77claralunagomez <claragomez968@gmail.com>
2025-04-24 18:38:21 -0300

    agregue verificacion en contacto

--- app/Views/contacto.php
+++ app/Views/contacto.php
@@ -1,26 +1,32 @@
 <section class="bg-light text-dark py-5">
-        <br>
-        <div class="text-center mb-5 mt-5 mx-5">
-            <h1>Contactanos</h1>
-            <h3">Mandanos un mensaje y te respondemos al correo lo antes posible</h3>
-        </div>
-        <br>
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-6 col-lg-4">
-                    <form>
-                        <div class=" mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Correo Electronico</label>
-                            <input type="email" class="form-control border-black" id="exampleInputEmail1" aria-describedby="emailHelp">
-                        </div>
-                        <br>
-                        <div class="mb-3">
-                            <label for="exampleFormControlTextarea1" class="form-label">Mensaje</label>
-                            <textarea class="form-control border-black" id="exampleFormControlTextarea1" rows="5"></textarea>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Enviar</button>
-                    </form>
-                </div>
+    <br>
+    <div class="text-center mb-5 mt-5 mx-5">
+        <h1>Contactanos</h1>
+        <h3>Mandanos un mensaje y te respondemos al correo lo antes posible</h3>
+    </div>
+    <br>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6 col-lg-4">
+                <form>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Correo Electrónico</label>
+                        <input type="email" class="form-control border-black" id="email" name="email" required>
+                    </div>
+                    <br>
+                    <div class="mb-3">
+                        <label for="mensaje" class="form-label">Mensaje</label>
+                        <textarea 
+                            class="form-control border-black" 
+                            id="mensaje" 
+                            name="mensaje" 
+                            rows="5" 
+                            required
+                        ></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Enviar</button>
+                </form>
             </div>
         </div>
-    </section>
+    </div>
+</section>
